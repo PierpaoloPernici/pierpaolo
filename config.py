0892afcb17dca4828b8410670cda64a195fec1b3
Pierpaolo Pernici <pierpaolo@gmail.com>
2012-09-20 17:44:20 +0200

    First commit

--- config.py
+++ config.py
@@ -15,13 +15,13 @@ BLOG = {
     "bloog_version": "0.8",
     "html_type": "text/html",
     "charset": "utf-8",
-    "title": "Bloog",
-    "author": "Bill Katz",
+    "title": "Pier'n Stuff",
+    "author": "Pierpaolo Pernici",
     # This must be the email address of a registered administrator for the 
     # application due to mail api restrictions.
-    "email": "you@foo.com",
-    "description": "A RESTful Blog/Homepage for Google AppEngine.",
-    "root_url": "http://bloog.billkatz.com",
+    "email": "pierpaolo@gmail.com",
+    "description": "... becouse I can't take photos 24h a day for a living",
+    "root_url": "http://10.90.0.196:8080",
     "master_atom_url": "/feeds/atom.xml",
     # By default, visitors can comment on article for this many days.
     # This can be overridden by setting article.allow_comments
@@ -61,46 +61,43 @@ PAGE = {
     "navlinks": [
         { "title": "Articles", "description": "Bits of Info", 
           "url": "/articles"},
-        { "title": "Contact", "description": "Send me a note", 
+        { "title": "Contact", "description": "Send me a pigeon", 
           "url": "/contact"},
     ],
     "featuredMyPages": {
-        "title": "Bloog Development",
-        "description": "Get involved",
+        "title": "Know me...",
+        "description": "...in the cyberspace",
         "entries": [
-            { "title": "Source Code", 
-              "url": "http://github.com/DocSavage/bloog", 
-              "description": "GitHub repository" },
-            { "title": "Tarball", 
-              "url": "http://github.com/DocSavage/bloog/tarball/master", 
-              "description": "Most recent snapshot" },
-            { "title": "Group", 
-              "url": "http://groups.google.com/group/bloog/topics", 
-              "description": "Developer discussion" },
-            { "title": "Author's Bloog", 
-              "url": "http://www.billkatz.com", 
-              "description": "What's brewing" },
-            { "title": "Architecture Diagram", 
-              "url": "/static/images/architecture2.png", 
-              "description": "RESTful Bloog" }
+            { "title": "Twitter", 
+              "url": "http://www.twitter.com/XniX", 
+              "description": "Twitter @XniX" },
+            { "title": "Facebook", 
+              "url": "http://facebook.com/pierpaolo.pernici", 
+              "description": "Facebook" },
+            { "title": "Google+", 
+              "url": "http://gplus.to/pierpaolpernici", 
+              "description": "Google+" },
+            { "title": "XDA", 
+	      "url": "http://forum.xda-developers.com/member.php?u=254321", 
+              "description": "XDA Forum" }
         ]
     },
-    "featuredOthersPages": {
-        "title": "Google App Engine",
-        "description": "Developer Resources",
-        "entries": [
-            { "title": "Google App Engine", 
-              "url": "http://code.google.com/appengine/", 
-              "description": "The mothership" },
-            { "title": "App Engine Group", 
-              "url": "http://groups.google.com/group/google-appengine", 
-              "description": "Developer group" },
-            { "title": "App Engine Open Source", 
-              "url": "http://groups.google.com/group/google-appengine/web/google-app-engine-open-source-projects", 
-              "description": "Code!" },
-            { "title": "App Engine Console", 
-              "url": "http://appengine.google.com", 
-              "description": "Your apps" }
-        ]
-    },
+#   "featuredOthersPages": {
+#       "title": "Google App Engine",
+#       "description": "Developer Resources",
+#       "entries": [
+#           { "title": "Google App Engine", 
+#             "url": "http://code.google.com/appengine/", 
+#             "description": "The mothership" },
+#           { "title": "App Engine Group", 
+#             "url": "http://groups.google.com/group/google-appengine", 
+#             "description": "Developer group" },
+#           { "title": "App Engine Open Source", 
+#             "url": "http://groups.google.com/group/google-appengine/web/google-app-engine-open-source-projects", 
+#             "description": "Code!" },
+#           { "title": "App Engine Console", 
+#             "url": "http://appengine.google.com", 
+#             "description": "Your apps" }
+#       ]
+#   },
 }
